520babb2069b3b3fb7742b3828958196852026cc
Pat Anderson <patrick.b.anderson@gmail.com>
2021-08-16 10:12:39 -0500

    POST route up and running, still working on GET

--- app.py
+++ app.py
@@ -1,51 +1,72 @@
-from flask import Flask, render_template, request, redirect, url_for
-from flask_sqlalchemy import SQLAlchemy
-import flask_sqlalchemy
+from flask import Flask, render_template, request, redirect, url_for, jsonify, flash
+import psycopg2
+from psycopg2.extras import RealDictCursor
 
 app = Flask(__name__)
-app.secret_key = "Secret Key"
 
-app.config['SQLALCHEMY DATABASE_URI'] = 'postgresql://patrickanderson@Localhost/zooapp'
-app.config['SQLALCHEMY TRACK_MODIFICATIONS'] = False
-
-db = SQLAlchemy(app)
-
-class Animal(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
-    species = db.Column(db.String(40))
-    age = db.Column(db.Integer)
-    gender = db.Column(db.String(20))
-    name = db.Column(db.String(40))
-    exhibit_id = db.Column(db.Integer)
-
-    def __init__(self, species, age, gender, name, exhibit_id):
-        self.species = species
-        self.age = age
-        self.gender = gender
-        self.name = name
-        self.exhibit_id = exhibit_id
-
-
+connection = psycopg2.connect(
+    host='Localhost',
+    port='5432',
+    database='zooapp'
+)
 
 @app.route('/')
 def Index():
     return render_template("index.html")
 
 # routes to db
+#GET
+@app.route('/api/animals', methods=['GET'])
+def list_animals():
+    #convert records to objects with RealDictCursor
+    cursor = connection.cursor(cursor_factory=RealDictCursor)
+    #query text
+    postgreSQL_select_Query = "SELECT * FROM animals;"
+    #send it over
+    cursor.execute(postgreSQL_select_Query)
+    #select rows
+    animals = cursor.fetchall()
+    #response
+    print( jsonify(animals) )
+    return jsonify(animals)
+
+#POST
 @app.route('/api/animals', methods=['POST'])
-def insert():
-    if request.method == 'POST':
-        species = request.form['species']
-        age = request.form['age']
-        gender = request.form['gender']
-        name = request.form['name']
-        exhibits_id = request.form['exhibit']
+def add():
+    species = request.form['species']
+    age = request.form['age']
+    gender = request.form['gender']
+    name = request.form['name']
+    exhibits_id = request.form['exhibit']
+    #SQL ALCHEMY STUFF:
+    # animal_data = Animal(species, age, gender, name, exhibits_id)
+    # db.session.add(animal_data)
+    # db.session.commit()
+    try:
+        cursor = connection.cursor(cursor_factory=RealDictCursor)
+        print(species, age, gender, name, exhibits_id)
+        queryText = "INSERT INTO animals (species, age, gender, name, exhibits_id) VALUES ( %s, %s, %s, %s, %s );"
+        cursor.execute(queryText, (species, age, gender, name, exhibits_id))
+        #commit the query
+        connection.commit()
+        count = cursor.rowcount
+        print(count, 'Animal added')
+        #response
+        result = { 'status' : 'CREATED' }
+        return jsonify(result), 201
+    except (Exception, psycopg2.Error) as err:
+        print( 'Add animal failed on server', err )
+        result = { 'status' : 'ERROR' }
+        return jsonify( result ), 500
+    finally:
+        if(cursor):
+            cursor.close()
 
-        animal_data = Animal(species, age, gender, name, exhibits_id)
-        db.session.add(animal_data)
-        db.session.commit()
+    flash("Animal added to your zoo")
 
-        return redirect(url_for('Index'))
+    return redirect(url_for('Index'))
+
+    
 
 if __name__ == "__main__":
     app.run(debug=True)
